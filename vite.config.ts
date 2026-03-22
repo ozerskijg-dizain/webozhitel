fd31a58633c8368de6cfc936b997b7c8850ea326
ozerskijg-dizain <ozerskijg@gmail.com>
2026-03-22 15:21:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({base: './',
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/webozhitel/" : "/",
   server: {
     host: "::",
     port: 8080,
