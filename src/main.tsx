3dea851980992730ac0981c392ebf619300a6043
ozerskijg-dizain <ozerskijg@gmail.com>
2026-03-23 14:28:49 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import App from "./App.tsx";
+import "./index.css";
+
+const basename = "/webozhitel/";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/webozhitel">
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
